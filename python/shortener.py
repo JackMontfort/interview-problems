625ec4d274bde1e8262220cfe4ea3981d4ac7bbe
JackMontfort <45523032+JackMontfort@users.noreply.github.com>
2018-12-07 11:11:39 -0500

    Add files via upload

--- python/shortener.py
+++ python/shortener.py
@@ -4,15 +4,11 @@
 
 # Remove any number of lowercase letters from s (or none at all), then capitalize all remaining letters of s.
 
-# Example of the rule:
-# s = AAbcD
-# Possible outputs using the rule:
-# Remove none, capitalize -> AABCD
-# Remove c, capitalize -> AABD
-# Remove b, capitalize -> AACD
-# Remove b and c, capitalize -> AAD
-
 # If it is possible to create the string t by processing s using the rule, then the function should return True, otherwise return False.
+
+#my general strategy was to compare the first character of s and t. If they are the same letter, then we drop both and move on to the next character of s and of t.
+#If they are not the same, we see if the character from s can be dropped. If not, return false. If we can, we do that and move on to the next character from s.
+#we do this until s and t are equal length. Then we simply compare s.upper() to t, and return true if they are the same and false if not.
 
 def shortener(s, t):
     while len(s)>len(t):
